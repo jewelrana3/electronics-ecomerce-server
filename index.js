62ddc7d9499caaa7ec40979f8d3cee10b06e60bb
Jewel Rana <juyelrana7752@gmail.com>
2023-11-07 20:22:22 +0600

    create intent payment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000
 
 // midleware
@@ -91,6 +91,19 @@ async function run() {
       res.send(result)
     })
 
+    // PAYMENT INTENT 
+    app.post('/create-payment-intent',async (req,res) =>{
+      const {price} = req.body;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount:amount,
+        currency:'usd',
+        payment_method_types:['card']
+      });
+      res.send({
+        clientSecret:paymentIntent.client_secret
+      })
+    })
 
 
     // Send a ping to confirm a successful connection
